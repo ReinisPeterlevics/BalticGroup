505770e313ba0da5b6cf684e55935b98ae2eaa02
Nopando <katerina220@inbox.lv>
2023-08-15 15:34:14 +0300

    just a messss

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,41 @@
-<footer>
+<!-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Travel app</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <link href="css/app.css" rel="stylesheet" >
+    </head>
+    <body> -->
+
+    <footer>
+    <div class="footer">
+        <div class="footer-section">
+            <h3>Contact Us</h3>
+            <p><strong>Email</strong>: info@example.com</p>
+            <p><strong>Phone</strong>: +371 12345678</p>
+            <p><strong>Address</strong>: 123 Travel street, City</p>
+        </div>
+        <div class="footer-section">
+            <h3>Follow Us</h3>
+            <p>
+                <!-- <i class="bi bi-facebook"></i> -->
+                <a href="https://www.facebook.com/magebitcom/">Facebook</a>
+            </p>
+            </p>
+                <!-- <i class="bi bi-twitter"></i> -->
+                <a href="https://twitter.com/magebit">Twitter</a></p>
+            <p>
+                <!-- <i class="bi bi-instagram"></i> -->
+                <a href="https://www.instagram.com/magebitcom/">Instagram</a>
+            </p>
+        </div>
+    </div>
+</footer>
+
+<!-- <footer>
     <div class="footer">
         <div class="row">
             <div class="col-md-9 mt-3">
@@ -9,7 +46,6 @@
             </div>
             <div class="col-md-5 mt-3 mb-3">
                 <h5 >Follow Us</h5>
-
                 <p class="mt-3 mb-2"><i class="bi bi-facebook me-2"><a href="https://www.facebook.com/magebitcom/"></i>Facebook</a></p>
                 <p class="mb-2"><i class="bi bi-twitter me-2"></i><a href="https://twitter.com/magebit"></i>Twitter</a></p>
                 <p><i class="bi bi-instagram me-2"></i><a href="https://www.instagram.com/magebitcom/">Instagram</a></p>
@@ -17,4 +53,8 @@
             </div>
         </div>
     </div>
-</footer>
+</footer> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html> -->
+
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,36 @@
+<!-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Travel app</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <link href="css/app.css" rel="stylesheet" >
+    </head>
+    <body> -->
 
-    <header>
+<header>
+    <div class="header">
+        <div class="cont">
+            <a class="navbar-brand" href="{{route('home')}}">LOGO</a>
+            <div class="navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('about')}}">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cart</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+
+    <!-- <header>
         <nav class="header navbar navbar-expand-lg">
             <div class="container-fluid ms-4 me-4">
                 <a class="navbar-brand" href="{{route('home')}}">LOGO</a>
@@ -15,6 +46,8 @@
                 </div>
             </div>
         </nav>
-    </header>
-
+    </header> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html> -->
 
