0365968f586b1663d264147535d85ca08c686355
Mufazzalshokh <mufazzalshokhulashev@gmail.com>
2023-08-15 09:31:43 +0300

    blog

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Blog</title>
+</head>
+<body>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis aut aperiam eveniet! Nostrum excepturi placeat libero, nam enim nulla modi dolor non eius esse in obcaecati iusto totam.
+         Assumenda autem harum officiis sapiente temporibus reprehenderit dicta nulla perspiciatis fugiat sunt eius ratione laborum, aut ipsam, sequi modi magni obcaecati.
+    </p>
+
+</body>
+</html>
