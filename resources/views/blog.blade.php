a48839e42334d6bcdeaad9758ad873ea6c11819d
Mufazzalshokh <mufazzalshokhulashev@gmail.com>
2023-08-15 15:14:56 +0300

    gri with column

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,9 +6,10 @@
     <link rel="stylesheet" href="css/blog.css">
     <title>Travel Blog</title>
 </head>
-<body>
+<body background="images/ocean.jpg.image_extension">
 
     <h1>Blog</h1>
+    <img sty src="" alt="">
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis aut aperiam eveniet! Nostrum excepturi placeat libero, nam enim nulla modi dolor non eius esse in obcaecati iusto totam.
          Assumenda autem harum officiis sapiente temporibus reprehenderit dicta nulla perspiciatis fugiat sunt eius ratione laborum, aut ipsam, sequi modi magni obcaecati.
@@ -23,126 +24,79 @@
         <div>
     </div>
 
-    <div class="container">
-        <div class="row row-bottom-padded-md">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
-                <div class="blog-text">
-                    <div class="prod-title">
-                        <h3>Planning for Vacation</h3>
-                        <span class="posted_by">Aug. 15th</span>
-                        <span class="comment">"21" <i class="icon-bubble2"></i></span>
-                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
-                            Temporibus iure dicta reiciendis odio!
-                        </p>
-                        <p>Learn more....</p>
 
+
+    <div class="row">
+        <div class="column">
+            <div class="container">
+                <div class="row row-bottom-padded-md">
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
+                        <div class="blog-text">
+                            <div class="prod-title">
+                                <h3>Planning for Vacation</h3>
+                                <span class="posted_by">Aug. 15th</span>
+                                <span class="comment">"21" <i class="icon-bubble2"></i></span>
+                                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
+                                    Temporibus iure dicta reiciendis odio!
+                                </p>
+                                <p>Learn more....</p>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="clearfix visible-sm-block"></div>
-    <div class="container">
-        <div class="row row-bottom-padded-md">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
-                <div class="blog-text">
-                    <div class="prod-title">
-                        <h3>Planning for Vacation</h3>
-                        <span class="posted_by">Aug. 15th</span>
-                        <span class="comment">"21" <i class="icon-bubble2"></i></span>
-                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
-                            Temporibus iure dicta reiciendis odio!
-                        </p>
-                        <p>Learn more....</p>
 
+        <div class="column">
+            <div class="container">
+                <div class="row row-bottom-padded-md">
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
+                        <div class="blog-text">
+                            <div class="prod-title">
+                                <h3>Planning for Vacation</h3>
+                                <span class="posted_by">Aug. 15th</span>
+                                <span class="comment">"21" <i class="icon-bubble2"></i></span>
+                                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
+                                    Temporibus iure dicta reiciendis odio!
+                                </p>
+                                <p>Learn more....</p>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="clearfix visible-sm-block"></div>
-    <div class="container">
-        <div class="row row-bottom-padded-md">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
-                <div class="blog-text">
-                    <div class="prod-title">
-                        <h3>Planning for Vacation</h3>
-                        <span class="posted_by">Aug. 15th</span>
-                        <span class="comment">"21" <i class="icon-bubble2"></i></span>
-                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
-                            Temporibus iure dicta reiciendis odio!
-                        </p>
-                        <p>Learn more....</p>
 
+        <div class="column">
+            <div class="container">
+                <div class="row row-bottom-padded-md">
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
+                        <div class="blog-text">
+                            <div class="prod-title">
+                                <h3>Planning for Vacation</h3>
+                                <span class="posted_by">Aug. 15th</span>
+                                <span class="comment">"21" <i class="icon-bubble2"></i></span>
+                                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
+                                    Temporibus iure dicta reiciendis odio!
+                                </p>
+                                <p>Learn more....</p>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="clearfix visible-sm-block"></div>
-    <div class="container">
-        <div class="row row-bottom-padded-md">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
-                <div class="blog-text">
-                    <div class="prod-title">
-                        <h3>Planning for Vacation</h3>
-                        <span class="posted_by">Aug. 15th</span>
-                        <span class="comment">"21" <i class="icon-bubble2"></i></span>
-                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
-                            Temporibus iure dicta reiciendis odio!
-                        </p>
-                        <p>Learn more....</p>
 
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
-    <div class="clearfix visible-sm-block"></div>
-    <div class="container">
-        <div class="row row-bottom-padded-md">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
-                <div class="blog-text">
-                    <div class="prod-title">
-                        <h3>Planning for Vacation</h3>
-                        <span class="posted_by">Aug. 15th</span>
-                        <span class="comment">"21" <i class="icon-bubble2"></i></span>
-                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
-                            Temporibus iure dicta reiciendis odio!
-                        </p>
-                        <p>Learn more....</p>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="clearfix visible-sm-block"></div>
-    <div class="container">
-        <div class="row row-bottom-padded-md">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <a href=""><img class="img-responsive" width="293" height="260" src="images/vacation.jpeg" alt="vacation image"></a>
-                <div class="blog-text">
-                    <div class="prod-title">
-                        <h3>Planning for Vacation</h3>
-                        <span class="posted_by">Aug. 15th</span>
-                        <span class="comment">"21" <i class="icon-bubble2"></i></span>
-                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus at itaque quibusdam commodi quia.
-                            Temporibus iure dicta reiciendis odio!
-                        </p>
-                        <p>Learn more....</p>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="clearfix visible-sm-block"></div>
+
 
 
 
