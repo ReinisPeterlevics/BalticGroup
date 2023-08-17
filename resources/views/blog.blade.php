050bf93506b1180093c3a81eb69e4b5e49554ee9
Mufazzalshokh <mufazzalshokhulashev@gmail.com>
2023-08-17 11:47:39 +0300

    ūpdate

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -10,23 +10,24 @@
 <body>
     @include('header')
 
-    <h1>Blog</h1>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis aut aperiam eveniet! Nostrum excepturi placeat libero, nam enim nulla modi dolor non eius esse in obcaecati iusto totam.
-         Assumenda autem harum officiis sapiente temporibus reprehenderit dicta nulla perspiciatis fugiat sunt eius ratione laborum, aut ipsam, sequi modi magni obcaecati.
-    </p>
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2 text-center">
-            <h3>Recent From Blog</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Blanditiis soluta modi vel quaerat magni ipsa animi nemo libero odio enim iusto, qui, expedita adipisci numquam? Quam sed ut odio ipsum?
-            </p>
-        </div>
-    </div>
 
 
     <div class="blog">
+    <h1>Blog</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur omnis aut aperiam eveniet! Nostrum excepturi placeat libero, nam enim nulla modi dolor non eius esse in obcaecati iusto totam.
+     Assumenda autem harum officiis sapiente temporibus reprehenderit dicta nulla perspiciatis fugiat sunt eius ratione laborum, aut ipsam, sequi modi magni obcaecati.
+</p>
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+        <h3>Recent From Blog</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Blanditiis soluta modi vel quaerat magni ipsa animi nemo libero odio enim iusto, qui, expedita adipisci numquam? Quam sed ut odio ipsum?
+        </p>
+    </div>
+</div>
         <div class="row">
             <div class="column">
                 <div class="container">
