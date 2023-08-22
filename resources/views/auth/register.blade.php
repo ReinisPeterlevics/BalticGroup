a0777500cdffe3a4a17917c5ef1e5dae5d8378f5
Mufazzalshokh <mufazzalshokhulashev@gmail.com>
2023-08-22 17:12:44 +0300

    fixed login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="css/location-description.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 
     <title>Travel app</title>
 </head>
