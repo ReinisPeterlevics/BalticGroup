7a5cabace354d88b7270d241901d3a90c9889aa8
Gundars <gundarens@gmail.com>
2023-08-22 16:55:26 +0300

    Merge remote-tracking branch 'origin' into auth

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -8,16 +8,59 @@ use Illuminate\Support\Facades\DB;
 
 class LocationController extends Controller
 {
-    public function index(){
-        $locations = DB::table('locations')->join('seasons', 'locations.season_id', '=','seasons.season_id')->join('countries', 'locations.country_id', '=', 'countries.country_id')->select('seasons.name as seasonname', 'countries.name as countryname', 'locations.*')->get();
+    public function index(Request $request){
+        $searchArr = $request->query->all();
+        $searchMap=array();
+        if($searchArr){
+            if(isset($searchArr['season'])){
+                $searchMap['locations.season_id'] = ['operator' => '=', "value" => $searchArr['season']];
+            }
+            if($searchArr['date-from']){
+                $searchMap['locations.start_date'] = ['operator' => '>=', "value" => $searchArr['date-from']];
+            }
+            if($searchArr['date-to']){
+                $searchMap['locations.end_date'] = ['operator' => '<=', "value" => $searchArr['date-to']];
+            }
+            if(isset($searchArr['country'])){
+                $searchMap['locations.country_id'] = ['operator' => '=', "value" => $searchArr['country']];
+            }
+            if($searchArr['budget']){
+                $searchMap['locations.price'] = ['operator' => '<=', "value" => $searchArr['budget']];
+            }
+            if($searchArr['persons']){
+                $searchMap['locations.spots_left'] = ['operator' => '>=', "value" => $searchArr['persons']];
+            }
+        }
+        $locations = DB::table('locations')
+                    ->join('seasons', 'locations.season_id', '=','seasons.season_id')
+                    ->join('countries', 'locations.country_id', '=', 'countries.country_id')
+                    ->select('seasons.name as seasonname', 'countries.name as countryname', 'locations.*')
+                    ->whereNested(function($query) use ($searchMap) {
+                        foreach ($searchMap as $key => $value){
+                            if ($value != ''){
+                                $query->where($key, $value["operator"], $value["value"]);
+                            }
+                        }
+                    }, 'and')
+                    ->get();
         return view('location', ['locations' => $locations]);
     }
     public function show($id){
-        $location = DB::table('locations')->join('seasons', 'locations.season_id', '=','seasons.season_id')->join('countries', 'locations.country_id', '=', 'countries.country_id')->select('seasons.name as seasonname', 'countries.name as countryname', 'locations.*')->where('locations.location_id', '=', $id)->get();
+        $location = DB::table('locations')
+                    ->join('seasons', 'locations.season_id', '=','seasons.season_id')
+                    ->join('countries', 'locations.country_id', '=', 'countries.country_id')
+                    ->select('seasons.name as seasonname', 'countries.name as countryname', 'locations.*')
+                    ->where('locations.location_id', '=', $id)
+                    ->get();
         return view('location-description', ['location' => $location]);
     }
     public function random(){
-        $locations = DB::table('locations')->join('seasons', 'locations.season_id', '=','seasons.season_id')->join('countries', 'locations.country_id', '=', 'countries.country_id')->select('seasons.name as seasonname', 'countries.name as countryname', 'locations.*')->inRandomOrder()->get();
+        $locations = DB::table('locations')
+                    ->join('seasons', 'locations.season_id', '=','seasons.season_id')
+                    ->join('countries', 'locations.country_id', '=', 'countries.country_id')
+                    ->select('seasons.name as seasonname', 'countries.name as countryname', 'locations.*')
+                    ->inRandomOrder()
+                    ->get();
         return view('location-description', ['location' => $locations]);
     }
 }
--- public/js/home.js
+++ public/js/home.js
@@ -2,3 +2,10 @@
 function rangeInputHandler(range) {
     document.getElementById(range.id + "Label").innerHTML = range.value;
 }
+// Reset select option
+function resetSelect(event, id) {
+    let key = (event.keyCode);
+    if (key === 8|| key === 46) {
+        document.getElementById(id).selectedIndex = -1;
+    }
+}
