7616878671c7c10aa580f86319f55de3cb27d1a6
Sandija-V <sandijaveze@gmail.com>
2023-08-23 17:46:16 +0300

    login and logout icons

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,31 +4,28 @@
             <a class="navbar-brand" href="{{route('home')}}"><img class="logoH" src="/images/finalLogo.png" alt="Magebit logo"></a>
             <div class="navbar-collapse">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" onclick="showCart()"><img src="/images/book4.png" alt="Travel Booking"></a>
-                    </li>
 
                     @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link-login" href="{{ route('login') }}"><img class="login-img" src="/images/login-icon3.png" alt="Login icon"></a>
                                 </li>
                             @endif
                         @else
-                    <li class="nav-item dropdown">
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
+                    <li class="nav-item ">
+                        <a class="nav-link-logout" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    <img class="logout-img" src="/images/logout-icon5.png" alt="">
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </li>
                     @endguest
+                    <li class="nav-item">
+                        <a class="nav-link-cart" href="#" onclick="showCart()"><img class="cart-img" src="/images/book4.png" alt="Travel Booking"></a>
+                    </li>
                 </ul>
             </div>
         </div>
